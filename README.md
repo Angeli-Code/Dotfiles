5c16191dc7a8015a7c0aaee2c2f810a6d32ccf84
Angelo Laberinto <ashhlabb@gmail.com>
2024-10-23 15:41:29 -0700

    Huge Update - added i3 configuration and finalized configs for a while - added documentation on decisions to README.md

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 A collection my personal configuration files
 
 ### Configuration File Contents
-- vimrc: Vim Text Editor
-- bashrc: Standard Shell
-- alacritty.toml: Terminal Emulator
-- tmux.conf: Terminal Multiplexor
+tmux.conf: config for Tmux. A terminal multiplexor. I use it for session management.
+vimrc: config for Vim. Pretty minimal with no LSP but it keeps me from getting distracted.
+bashrc: config for my bash shell. Pretty minimal. Only added vim motions and a custom PS1
+config: config for i3 settings. Removes borders and starts other processes like polybar and a custom background
+config.ini:c config for polybar. A customized taskbar for i3.
+touchscreen.conf: config file that disables my laptops touch screen. 
+alacritty.toml: config for my terminal emulator.
+
+### Distrobutions
+Fedora has always been super stable for me. I have used Ubuntu, Arch, Debian, Manjaro, and PopOS. Fedora has 
+provided a more modern but stable experience and I plan on staying here for a long time. 
 
 ### Scripts
 - fedora-install-script: Automated installation of packages and file sorting
 automation of how I like my setup.
+- obsidian: automates opening obsidian app image. Allowing it to be opened through dmenu
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+# fedora i3 Configuration
 
 # General Package Installation
 sudo dnf update -y
-sudo dnf install -y git svn xsel gdb valgrind make gcc g++ neofetch qemu nasm
-sudo dnf install -y kernel-devel kernel-headers ctags tmux java-21-openjdk-devel.x86_64 
-sudo dnf install -y ncurses-devel bc bison flex elfutils-libelf-devel openssl-devel dwarves
-sudo dnf install -y alacritty
+sudo dnf install -y git svn xsel gdb valgrind make gcc g++ neofetch qemu nasm java-21-openjdk-devel.x86_64
+sudo dnf install -y tmux ncurses-devel bc bison flex openssl-devel dwarves elfutils-libelf-devel
+sudo dnf install -y alacritty cscope ctags polybar brightnessctl vim obs-studio
 
-# Arduino-CLI
+# Arduino-CLI 
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 cd bin/
 sudo mv arduino-cli /usr/local/bin
@@ -18,9 +18,9 @@ arduino-cli config init
 arduino-cli config dump
 arduino-cli core install arduino:avr
 arduino-cli core update-index
-arduino-cli core updgrade
+arduino-cli core upgrade
 
-# Linux Kernel
+# Linux Kernel Cloning,Compiling,and Ctags generation
 cd
 git clone https://github.com/torvalds/linux
 cd linux
@@ -29,39 +29,33 @@ make -j$(nproc)
 cd
 mv ~/linux/ ~/Programming
 cd ~/Programming/linux/
+ctags -R .
 
-# Configure Git
-git config --global user.name "Enter Name Here"
-git config --global user.email "Enter Email Here"
+# Git configuration
+git config --global user.name "Enter name here"
+git config --global user.email "Enter email here"
 git config --global core.editor vi
 git config --global pull.rebase true  
 git config --global init.defaultBranch main  
-git config --global color.ui auto 
+git config --global color.ui auto
 
-# Move Files Into Correct Locations
-mkdir ~/.config/alacritty/
-mkdir ~/.vim/
-mkdir ~/.vim/undo/
-cp ~/Programming/Dotfiles/Config-Files/vim ~/.vimrc
-cp ~/Programming/Dotfiles/Config-Files/bashrc ~/.bashrc
+# Move Files into Correct Locations
+mkdir -p ~/.config/alacritty/
+mkdir -p ~/.config/polybar/
+mkdir -p ~/.vim/
+mkdir -p ~/.vim/undo/
+cp ~/Programming/Dotfiles/Config-Files/vimrc ~/.vimrc
 cp ~/Programming/Dotfiles/Config-Files/tmux.conf ~/.tmux.conf
-cp ~/Programming/Dotfiles/Config-Files/alacritty.toml ~/.config/alacritty/
-sudo cp ~/Programming/Dotfiles/Config-Files/touchscreen.conf /etc/X11/xorg.conf.d/
+cp ~/Programming/Dotfiles/Config-Files/alacritty.toml ~/.config/alacritty/alacritty.toml
+cp ~/Programming/Dotfiles/Config-Files/config.ini ~/.config/polybar/config.ini
+cp ~/Programming/Dotfiles/Config-Files/config ~/.config/i3/config
+cp ~/Programming/Dotfiles/Config-Files/bashrc ~/.bashrc
+sudo cp ~/Programming/Dotfiles/Config-Files/touchscreen.conf /etc/X11/xorg.conf.d/touchscreen.conf
 
-# Remove Unnecessary Files
-sudo dnf -y remove rhythmbox
-sudo dnf -y remove libreoffice*
-sudo dnf -y remove flatpak
-sudo dnf -y remove gnome-terminal
-sudo dnf -y autoremove
-sudo dnf -y clean all
+# Wallpaper 
+mv ~/Programming/Dotfiles/wallpaper.jpg ~/Pictures/wallpaper.jpg
 
-# Basic File Management
-cd ~/
-mkdir Media/
-rm -r Desktop/ Public/ Templates/ Music/
-mv Videos/ Pictures/ Media/
-cd ~/
-mv linux ~/Programming/
-
-reboot
+# Clean up
+sudo dnf remove i3-sensible-terminal
+sudo dnf autoremove -y
+sudo dnf clean all
